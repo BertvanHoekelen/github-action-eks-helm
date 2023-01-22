d81e3cc9286e1620e3ee268be64330db45e73c2d
Bert van Hoekelen <bert@magwel.nl>
2023-01-22 17:17:44 +0100

    Echo result

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 chmod 644 kubeconfig
 export KUBECONFIG=kubeconfig
 
+result="$($1)"
 status=$?
+
+echo result
 
 if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
